e6205b8cf2548549c0ee307f340ee064f5d4b3dd
Rollulus <roelboel@xs4all.nl>
2016-04-25 15:16:16 +0200

    Tear mapper logic apart for logging

--- src/main/scala/com/eneco/trading/kafka/streams/csv/StreamProcessOperations.scala
+++ src/main/scala/com/eneco/trading/kafka/streams/csv/StreamProcessOperations.scala
@@ -9,7 +9,7 @@ import scala.util.{Success, Failure, Try}
 import org.apache.avro.Schema
 
 trait CsvToGenericRecordMapper {
-  def map(csvTokens: Seq[String]): Try[GenericRecord]
+  def toGenericRecord(csvTokens: Seq[String]): Try[GenericRecord]
 }
 
 trait CsvLineTokenizer {
@@ -20,29 +20,28 @@ class LameCommaCsvTokenizer extends CsvLineTokenizer {
   def tokenize(r: String): Seq[String] = r.split(",").map(_.trim).toSeq
 }
 
-class SchemaDrivenMapper(schema: Schema) extends CsvToGenericRecordMapper {
+class SchemaDrivenMapper(schema: Schema) extends CsvToGenericRecordMapper with Logging {
   require(schema.getType == Type.RECORD)
 
-  val fieldMappers = schema.getFields.asScala.toSeq.map(field => {
-    val fieldType = if (field.schema.getType == Type.UNION) {
+  val fields = schema.getFields.asScala.toSeq
+  val fieldTypes = fields.map(field => if (field.schema.getType == Type.UNION)
         field.schema.getTypes.asScala.toSeq.map(_.getType) match {
         case Seq(Type.NULL, sect) => sect
         case _ => throw new Exception(s"Only unions of type [null, t] are supported, not `${field.schema}`")
-      }
-    } else {
-      field.schema.getType
-    }
-    fieldType match {
-      case Type.STRING => (s: String) => (field.name, s)
-      case Type.INT => (s: String) => (field.name, s.toInt)
-      case Type.FLOAT => (s: String) => (field.name, s.toFloat)
-      case Type.DOUBLE => (s: String) => (field.name, s.toDouble)
-      case Type.BOOLEAN => (s: String) => (field.name, s.toBoolean)
-      case _ => throw new Exception(s"cannot map unsupported type `${field.schema}`")
-    }
-  })
+      } else field.schema.getType)
 
-  def map(csvTokens: Seq[String]): Try[GenericRecord] = {
+  log.info("the mapping is: " + fields.zip(fieldTypes).map{case(f,t) => s"${f.name}:${t.getName}"}.mkString(", "))
+
+  val fieldMappers = fields.zip(fieldTypes).map { case (field, fieldType) => fieldType match {
+    case Type.STRING => (s: String) => (field.name, s)
+    case Type.INT => (s: String) => (field.name, s.toInt)
+    case Type.FLOAT => (s: String) => (field.name, s.toFloat)
+    case Type.DOUBLE => (s: String) => (field.name, s.toDouble)
+    case Type.BOOLEAN => (s: String) => (field.name, s.toBoolean)
+    case _ => throw new Exception(s"cannot map `${field.name}` unsupported type `${fieldType}`")
+  }}
+
+  def toGenericRecord(csvTokens: Seq[String]): Try[GenericRecord] = {
     if (csvTokens.length != fieldMappers.length) {
       return Failure(new IllegalArgumentException(s"number of csv tokens ${csvTokens.length} does not match number of record fields ${fieldMappers.length}"))
     }
@@ -61,7 +60,7 @@ class StreamingOperations(mapper: CsvToGenericRecordMapper) extends Logging {
   val tokenizer = new LameCommaCsvTokenizer
 
   private def mapOrLogError(s: String) = {
-    val v = mapper.map(tokenizer.tokenize(s))
+    val v = mapper.toGenericRecord(tokenizer.tokenize(s))
     v match {
       case Failure(m) => log.warn(s"failed to parse `${s}`: ${m.toString}")
       case _ =>
